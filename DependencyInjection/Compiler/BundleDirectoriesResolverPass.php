26e5b8e66ef5842a0f64f67343d0118dd2d1d935
Maxime Ailloud <maxime.ailloud@gmail.com>
2020-05-28 11:28:33 -0400

    Fix for bundleless application

--- DependencyInjection/Compiler/BundleDirectoriesResolverPass.php
+++ DependencyInjection/Compiler/BundleDirectoriesResolverPass.php
@@ -7,9 +7,6 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\Definition;
 
 /**
- * BundleDirectoriesResolverPass
- *
- * @uses CompilerPassInterface
  * @author Stephane PY <py.stephane1@gmail.com>
  */
 class BundleDirectoriesResolverPass implements CompilerPassInterface
@@ -21,17 +18,17 @@ class BundleDirectoriesResolverPass implements CompilerPassInterface
      */
     public function process(ContainerBuilder $container)
     {
-        $bundles         = $container->getParameter('kernel.bundles');
+        $bundles = $container->getParameter('kernel.bundles');
         $bundleContainer = $container->getDefinition('atoum.configuration.bundle.container');
-        $configuration   = $container->getParameterBag()->resolveValue($container->getParameter('atoum.bundles'));
+        $configuration = $container->getParameterBag()->resolveValue($container->getParameter('atoum.bundles'));
 
         foreach ($configuration as $bundleName => $data) {
-            if (!isset($bundles[$bundleName])) {
+            if ($data['is_bundle'] && !isset($bundles[$bundleName])) {
                 throw new \LogicException(sprintf('Bundle "%s" does not exists.', $bundleName));
             }
 
-            $rc        = new \ReflectionClass($bundles[$bundleName]);
-            $directory = dirname($rc->getFileName());
+            $reflectionClass = new \ReflectionClass($data['is_bundle'] ? $bundles[$bundleName] : 'App\Kernel');
+            $directory = dirname($reflectionClass->getFileName());
 
             $directories = array_map(
                 function ($v) use ($directory) {
